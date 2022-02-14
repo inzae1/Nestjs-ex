0df85de367e283c91c3b5ac611e9b0348e804aa7
inzae <inzae@datacrush.ai>
2022-02-14 12:55:49 +0900

    220214 서비스업데이트

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,20 +1,46 @@
 import { Injectable } from '@nestjs/common';
 import { Board, BoardStatus } from './board.model';
+import { CreateBoardDto } from './dto/create-board.dto';
+import { v1 as uuid } from 'uuid';
 
 @Injectable()
 export class BoardsService {
   private boards: Board[] = [];
 
+  // 모든 게시물 가져오기
   getAllBoards(): Board[] {
     return this.boards;
   }
 
-  createBoard(title: string, description: string) {
+  // 게시물 만들기
+  createBoard(createBoardDto: CreateBoardDto): Board {
+    const { title, description } = createBoardDto;
+
     const board: Board = {
       id: uuid(),
       title,
       description,
       status: BoardStatus.PUBLIC,
     };
+
+    this.boards.push(board);
+    return board;
+  }
+
+  // ID로 게시물 가져오기
+  getBoardById(id: string): Board {
+    return this.boards.find((board) => board.id === id);
+  }
+
+  // ID로 게시물 지우기
+  deleteBoard(id: string): void {
+    this.boards = this.boards.filter((board) => board.id !== id);
+  }
+
+  // ID로 상태업데이트
+  updateBoardStatus(id: string, status: BoardStatus): Board {
+    const board = this.getBoardById(id);
+    board.status = status;
+    return board;
   }
 }
